f073d1c84d1c3dde123ef5d7584352da7d24a320
MeeraMenon1807 <91048876+MeeraMenon1807@users.noreply.github.com>
2022-02-23 23:54:05 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -3,6 +3,6 @@
 #include "batterycharacter.h"
 #include <stdio.h>
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+BreachType inferBreach(double value, double lowerLimit, double upperLimit); 
 
 void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
